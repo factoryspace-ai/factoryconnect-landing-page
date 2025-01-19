039f2bb820cda03e41e0fd0c6979cbb6a51936a4
Jyoti Prakash Panda <96424607+jyotiprakashh@users.noreply.github.com>
2025-01-19 20:15:41 +0530

    fix errors

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar() {
     <header className="w-full py-4 border-b border-gray-700 text-white">
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 flex justify-between items-center">
         <div className="flex items-center">
-          <span className="font-semibold text-xl">FactorySpace</span>
+          <span className="font-semibold text-xl">FactoryConnect</span>
         </div>
 
         <div className="flex items-center gap-6">
