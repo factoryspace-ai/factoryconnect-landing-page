161ae7f73464ed48f01296bd80be3441095ec9ae
Jyoti Prakash Panda <96424607+jyotiprakashh@users.noreply.github.com>
2025-01-19 19:41:39 +0530

    fix errors

--- app/api/msme/route.ts
+++ app/api/msme/route.ts
@@ -1,20 +1,26 @@
 import { NextResponse } from "next/server";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { getMsmesByUserEmail } from "@/lib/msme-utils";
 
 export async function GET() {
   try {
     const { userId } =await auth();
-    const user = await currentUser();
+    const response = await fetch(`https://api.clerk.dev/v1/users/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
+      },
+    });
+
+    const user = await response.json();
 
     if (!userId || !user) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const userEmail = user.emailAddresses[0].emailAddress.toLowerCase();
+    const userEmail = user.email_addresses[0].email_address;
     const userMsmes = await getMsmesByUserEmail(userEmail);
 
-    if (!userMsmes || userMsmes.length === 0) {
+    if (!userMsmes || userMsmes.length == 0) {
       return new NextResponse("No MSMEs found for user", { status: 404 });
     }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
-import { UserCreator } from "@/components/auth/user-creator";
+// import { UserCreator } from "@/components/auth/user-creator";
 import { Toaster } from "sonner";
 
 const geistSans = localFont({
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ClerkProvider>
           <Toaster />
-          <UserCreator />
+          {/* <UserCreator /> */}
           {children}
         </ClerkProvider>
       </body>
